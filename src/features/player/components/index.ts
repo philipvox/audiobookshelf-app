a2e3e858c746509e20285a38aea22c88a0e254cb
philipvox <philipvox@users.noreply.github.com>
2025-12-01 12:59:18 -0800

    improved design of player

--- src/features/player/components/index.ts
+++ src/features/player/components/index.ts
@@ -2,8 +2,10 @@
  * src/features/player/components/index.ts
  */
 
-export { CustomSlider } from './CustomSlider';
+export { GlassPanel, type LightingVariant } from './GlassPanel';
+export { PlayerButton, type ButtonVariant } from './PlayerButton';
+export { CassetteTape } from './CassetteTape';
+export { MiniPlayer } from './MiniPlayer';
 export { AudioWaveform } from './AudioWaveform';
 export { PlayerHeader } from './PlayerHeader';
-export { PlayerControls } from './PlayerControls';
 export { PlayerProgress } from './PlayerProgress';
